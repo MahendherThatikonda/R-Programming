04dd9a7d1483f1b5d2c7a328e2de72a3d9128b17
MahendherThatikonda <116361201+MahendherThatikonda@users.noreply.github.com>
2024-07-20 13:29:40 +1200

    First Program in R
    Beginner programs in Learn R

---
+++ FirstR.R
@@ -0,0 +1,60 @@
+# Basic Programs in R
+var <-5
+var1 <-10
+var2<-var*var1 #Multiplication
+
+var3<-var + var1 #Addition
+
+varsub <-var1 - var #Subtraction
+
+vardiv <- var1/var #Division
+
+varx= "Hello World"
+
+varx=10
+
+print("Hello World", 10) #Print Function to print something
+print(10)
+#for(x=1;x<=10;x)
+for(x in 1:10){
+   y <- 5*x
+   print(y)
+}
+
+
+name ="Mahendher"
+
+name
+
+for(x in 1:10){
+  y<-"5*x"
+paste("5 X x =",y)
+}
+
+paste(name,"Mahi")
+
+for(z in 1:10){
+  y<-10*z
+  print("10 x \"z\"=",y)
+}
+
+paste("Mahi",5)
+
+
+x<-y<-z<-10
+x
+y
+z
+
+z<-125L
+y<-"Mahi"
+class(y)
+class(x)
+class(z)
+
+sd<-as.numeric(z)
+sd
+
+cd<-sqrt(24)
+ceil<-ceiling(1.4)
+floo<-floor(1.4)
